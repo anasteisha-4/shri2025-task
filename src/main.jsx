09576f06f03b4be4676a148b92bf951d8b7b0e41
Bronina A.O <bronina04@mail.ru>
2025-07-02 22:52:01 +0300

    fix: style fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './reset.css';
-import './fonts.css';
-import './index.css';
 
 createRoot(document.getElementById('app')).render(
   <StrictMode>
